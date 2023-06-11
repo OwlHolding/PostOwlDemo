f0496fb79f81a0fec31603fbe38c6e2c950391ec
romantihiy <romantihomirov1@gmail.com>
2023-06-11 20:52:58 +0300

    Added section in database

--- bot.py
+++ bot.py
@@ -10,39 +10,39 @@ with open('config.json') as f:
 
 con = sqlite3.connect('database.db', check_same_thread=False)
 
-def process_response(message: telebot.types.Message, group: str):
-    if message.text == config[group]['buttonOK']:
-        bot.send_message(message.chat.id, config[group]['messageOK'])
+def process_response(message: telebot.types.Message, section: str):
+    if message.text == config[section]['buttonOK']:
+        bot.send_message(message.chat.id, config[section]['messageOK'])
         with con:
             con.executemany(
                 'UPDATE users SET status="subscribed" WHERE id=?', [(message.chat.id,)])
-    elif message.text == config[group]['buttonCancel']:
-        bot.send_message(message.chat.id, config[group]['messageCancel'])
+    elif message.text == config[section]['buttonCancel']:
+        bot.send_message(message.chat.id, config[section]['messageCancel'])
         with con:
             con.executemany(
                 'UPDATE users SET status="refused" WHERE id=?', [(message.chat.id,)])
     else:
-        bot.send_message(message.chat.id, config[group]['messageUnknown'])
-        bot.register_next_step_handler(message, process_response, group=group)   
+        bot.send_message(message.chat.id, config[section]['messageUnknown'])
+        bot.register_next_step_handler(message, process_response, section=section)   
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message: telebot.types.Message):
-    group = message.text.split(' ')
-    if len(group) > 1:
-        group = group[1]
+    section = message.text.split(' ')
+    if len(section) > 1:
+        section = section[1]
     else:
-        group = 'default'
+        section = 'default'
     try:
         keyboard = telebot.types.ReplyKeyboardMarkup(
             resize_keyboard=True, one_time_keyboard=True)
-        keyboard.add(config[group]['buttonOK'], config[group]['buttonCancel'])
+        keyboard.add(config[section]['buttonOK'], config[section]['buttonCancel'])
         bot.send_message(
-            message.chat.id, config[group]['messageHello'], reply_markup=keyboard)
+            message.chat.id, config[section]['messageHello'], reply_markup=keyboard)
         with con:
             con.executemany(
-                'INSERT OR REPLACE INTO users (id, username, status) values(?, ?, ?)', 
-                [(message.chat.id, message.chat.username, "launched")])
-        bot.register_next_step_handler(message, process_response, group=group)
+                'INSERT OR REPLACE INTO users (id, username, section, status) values(?, ?, ?, ?)', 
+                [(message.chat.id, message.chat.username, section, "launched")])
+        bot.register_next_step_handler(message, process_response, section=section)
     except KeyError:
         pass
 
