bbd0296d5e3f725249efd34f6f252419553fa794
Kirill72-web <kirill.lego-kot@ya.ru>
2023-06-13 09:51:49 +0300

    rewrite webhook handler

--- app.py
+++ app.py
@@ -12,11 +12,16 @@ app = web.Application()
 
 
 async def handler(request):
-    request_body = await request.json()
-    update = telebot.types.Update.de_json(request_body)
-    bot.process_new_updates([update])
+    if request.match_info.get("token") == bot.token:
+        request_body_dict = await request.json()
+        update = telebot.types.Update.de_json(request_body_dict)
+        bot.process_new_updates([update])
+        return web.Response()
+    else:
+        return web.Response(status=403)
 
-app.router.add_post("/", handler)
+
+app.router.add_post("/{token}/", handler)
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain(ssl_config['ssl-cert'], ssl_config['ssl-private'])
